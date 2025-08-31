ce4726632d89f232bc5e23c6bd984a5bcf37058e
Sourabh-Web21 <artistbros007@gmail.com>
2025-08-31 15:54:23 +0530

    fixed search bar

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,7 +2,7 @@ const search = ({searchTerm,setSearchTerm}) => {
   return (
     <div className="search flex" > 
         {/* <FaSearch size={20} color="gold" /> */}
-      <input type="text" className="w-0.5" placeholder="Search your movie here:" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}></input>
+      <input type="text" className=" bg-amber-50 w-3" placeholder="Search your movie here:" value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}></input>
      <button
         onClick={() => setSearchTerm('')}
         className="text-white bg-gray-100 h-fit w-6 rounded-2xl my-3"
